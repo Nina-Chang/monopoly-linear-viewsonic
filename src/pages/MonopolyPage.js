f372ec66d63eb02b0811ac57f2647c8f9f447a49
Nina <nina2000827@gmail.com>
2026-01-24 13:57:06 +0800

    MonopolyPage's appearance done

--- src/pages/MonopolyPage.js
+++ src/pages/MonopolyPage.js
@@ -12,6 +12,75 @@ const MonopolyPage = ({navigateTo, backgroundImage}) => {
             <button onClick={()=>{navigateTo("scores")}}>
                 navigateTo
             </button>
+            <span className="step-1">
+                <img src="./images/object/Basketball_monopoly_GO.png" alt="" />
+            </span>
+            <span className="step-2">
+                Question
+            </span>
+            <span className="step-3">
+                Question
+            </span>
+            <span className="step-4">
+                Question
+            </span>
+            <span className="step-5">
+                Question
+            </span>
+            <span className="step-6">
+                Question
+            </span>
+            <span className="step-7">
+                Question
+            </span>
+            <span className="step-8">
+                <img src="./images/object/Basketball_monopoly_question_mark.png" alt="" />
+            </span>
+            <span className="step-9">
+                Question
+            </span>
+            <span className="step-10">
+                Question
+            </span>
+            <span className="step-11">
+                <img src="./images/object/Basketball_monopoly_treasure_chest.png" alt="" />
+            </span>
+            <span className="step-12">
+                Question
+            </span>
+            <span className="step-13">
+                Question
+            </span>
+            <span className="step-14">
+                Question
+            </span>
+            <span className="step-15">
+                Question
+            </span>
+            <span className="step-16">
+                Question
+            </span>
+            <span className="step-17">
+                Question
+            </span>
+            <span className="step-18">
+                Question
+            </span>
+            <span className="step-19">
+                Question
+            </span>
+            <span className="step-20">
+                Question
+            </span>
+            <span className="step-21">
+                Question
+            </span>
+            <span className="step-22">
+                Question
+            </span>
+            <span className="step-23">
+                Question
+            </span>
         </div>
     )
 }
